0ba3c5ad90f6160d6482abe77308fbecbb83270f
creategamer <creategamer10@gmail.com>
2024-08-13 18:20:20 +0530

    comments this

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import styled, { ThemeProvider } from "styled-components"
 import Menu from "./componenets/Menu";
 import Navbar from "./componenets/Navbar";
-import { darkTheme, lightTheme } from "./utils/Theme";
+import { darkTheme, lightTheme } from "./utils/Theme.js";
 import { useState } from "react";
 import {
   BrowserRouter,
@@ -31,115 +31,115 @@ import History from "./History/History";
 import Search from "./pages/Search";
 import SubscribedVideo from "./subscribedVideo/SubscribedVideo";
 import NewsComponent from "./News/NewsComponets";
-import Music from "../../mediahub/models/Music";
+// import Music from "../../mediahub/models/Music";
 import MusicComponents from "./Music/MusicComponents";
 import UploadMusic from "./Music/UploadMusic";
 import MusicPart from "./Music/MusicPart";
 import ForgotPassword from "./componenets/ForgotPassword";
 
 
-const Container=styled.div`
+const Container = styled.div`
   display:flex;
 `;
 
-const Main= styled.div`
+const Main = styled.div`
   flex:7;
-  background-color:${({theme})=>theme.bg};
+  background-color:${({ theme }) => theme.bg};
   
 `;
-const Wrapper=styled.div`
+const Wrapper = styled.div`
   padding:22px 96px;
 `;
 
 function App() {
-  
-  const [darkMode,setDarkMode]=useState(true)
+
+  const [darkMode, setDarkMode] = useState(true)
 
   const { currentUser } = useSelector((state) => state.user);
 
-  return(
-  <ThemeProvider theme={darkMode ? darkTheme : lightTheme}> 
-  <Container>
-    <BrowserRouter>    
-    <Menu darkMode={darkMode} setDarkMode={setDarkMode}/>
-    <Main>
-      <Navbar/>
-      <Wrapper>
-        <Routes>
-          <Route path="/">
-              <Route index  element={<Home type="random"/>} />
-              <Route path="trends" element={<Home type="trend"/>} />
-              {/* <Route path="subscriptions" element={<Home type="sub"/>} /> */}
-              <Route path="Help" element={<Help/>}/>
-              <Route path="signup" element={<Signup />} /> {/* Move Signup route outside of Signin route */}
-              
-              {/* <Route
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Container>
+        <BrowserRouter>
+          <Menu darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Navbar />
+            <Wrapper>
+              <Routes>
+                <Route path="/">
+                  <Route index element={<Home type="random" />} />
+                  <Route path="trends" element={<Home type="trend" />} />
+                  {/* <Route path="subscriptions" element={<Home type="sub"/>} /> */}
+                  <Route path="Help" element={<Help />} />
+                  <Route path="signup" element={<Signup />} /> {/* Move Signup route outside of Signin route */}
+
+                  {/* <Route
                     path="signin"
                     element={currentUser ? <Home type="random"/> : <Signin />}
               > */}
 
-              <Route
-                path="signin"
-                element={
-                  currentUser ? (
-                    currentUser.name === "Admin" ? (
-                      <AdminHome/>
-                    ) : (
-                      <Home type="random" />
-                    )
-                  ) : (
-                    <Signin />
-                  )
-                }
-              >
+                  <Route
+                    path="signin"
+                    element={
+                      currentUser ? (
+                        currentUser.name === "Admin" ? (
+                          <AdminHome />
+                        ) : (
+                          <Home type="random" />
+                        )
+                      ) : (
+                        <Signin />
+                      )
+                    }
+                  >
 
-              </Route>
-            <Route path="blogs" element={<Blogs/>}/>
-            <Route path="bloging">
-              {/* blog/find */}
-              <Route path=":id" element={<Bloging/>} />    
-            </Route>
-            <Route path="uploadBlogs" element={<UploadBlogs/>}/>
-            <Route path="video">
-              {/* videos find/find */}
-              <Route path=":id" element={<Video/>} />    
-            </Route>
-            {/* <Route path="tweet" element={<Tweet/>}/> */}
-            <Route path="tweet" element={<Tweets/>}/>
-            <Route path="ForgotPassword" element={<ForgotPassword/>}/>
-            
-            <Route path="tweet" element={<Tweets/>}/>
-            <Route path="music" element={<MusicComponents/>}/>
-            <Route path="musicPart">
-              {/* music/find */}
-              <Route path=":id" element={<MusicPart/>} />    
-            </Route>
-            <Route path="uploadmusic" element={<UploadMusic/>}/>
-            
-            <Route path="News" element={<NewsComponent/>}/>
+                  </Route>
+                  <Route path="blogs" element={<Blogs />} />
+                  <Route path="bloging">
+                    {/* blog/find */}
+                    <Route path=":id" element={<Bloging />} />
+                  </Route>
+                  <Route path="uploadBlogs" element={<UploadBlogs />} />
+                  <Route path="video">
+                    {/* videos find/find */}
+                    <Route path=":id" element={<Video />} />
+                  </Route>
+                  {/* <Route path="tweet" element={<Tweet/>}/> */}
+                  <Route path="tweet" element={<Tweets />} />
+                  <Route path="ForgotPassword" element={<ForgotPassword />} />
 
-            <Route path="profile" element={<ProfilePage/>}/>
-            <Route path="subs" element={<Subscription/>}/>
-            {/* <Route path="Library" element={<Library/>}/> */}
-            <Route path="Library" element={<SubscribedVideo/>}/>
-            <Route path="history" element={<History/>}/>
-                
-            <Route path="feedback" element={<FeedbackPage/>}/>
-            
-            <Route path="AdminHome" element={<AdminHome/>}/>
-            
-            <Route path="UserProfile" element={<AdminUserPanel/>}/>
-            <Route path="AdminProfile" element={<AdminProfile/>}/>
-            <Route path="basicControls" element={<BasicControls/>}/>
-            <Route path="search" element={<Search/>} />
-          </Route>
-        </Routes>
-      </Wrapper>
-    </Main>
-    </BrowserRouter>
-  </Container>
+                  <Route path="tweet" element={<Tweets />} />
+                  <Route path="music" element={<MusicComponents />} />
+                  <Route path="musicPart">
+                    {/* music/find */}
+                    <Route path=":id" element={<MusicPart />} />
+                  </Route>
+                  <Route path="uploadmusic" element={<UploadMusic />} />
 
-  </ThemeProvider>
+                  <Route path="News" element={<NewsComponent />} />
+
+                  <Route path="profile" element={<ProfilePage />} />
+                  <Route path="subs" element={<Subscription />} />
+                  {/* <Route path="Library" element={<Library/>}/> */}
+                  <Route path="Library" element={<SubscribedVideo />} />
+                  <Route path="history" element={<History />} />
+
+                  <Route path="feedback" element={<FeedbackPage />} />
+
+                  <Route path="AdminHome" element={<AdminHome />} />
+
+                  <Route path="UserProfile" element={<AdminUserPanel />} />
+                  <Route path="AdminProfile" element={<AdminProfile />} />
+                  <Route path="basicControls" element={<BasicControls />} />
+                  <Route path="search" element={<Search />} />
+                </Route>
+              </Routes>
+            </Wrapper>
+          </Main>
+        </BrowserRouter>
+      </Container>
+
+    </ThemeProvider>
   )
 }
 
