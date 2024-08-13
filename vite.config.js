8afa801da9e44940692ef03cdbbeb938f7880ed3
creategamer <creategamer10@gmail.com>
2024-08-13 18:10:47 +0530

    this is final comments

--- vite.config.js
+++ vite.config.js
@@ -3,14 +3,15 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server:{
+  server: {
     // proxy:{
     //   '/api':'http://localhost:8800',
     // },
     proxy: {
       '/api': {
         // Replace with your API server URL
-        target: 'http://localhost:8800/api',
+        // target: 'http://localhost:8800/api',
+        target: 'https://mediahubbackend.onrender.com/api',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
